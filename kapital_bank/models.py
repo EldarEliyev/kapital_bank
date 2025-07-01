9c45df67bf0271c49fee8cdf30f6448bcaf1711f
eldar23451 <liyev8024@gmail.com>
2025-07-01 15:57:27 +0400

    Updated __str__ method in KapitalBank model

--- kapital_bank/models.py
+++ kapital_bank/models.py
@@ -38,6 +38,6 @@ class KapitalBank(models.Model):
     fin_code = models.CharField(max_length=10)
     address = models.CharField(max_length=30)
     birth_date = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
-        return self.user
+      return str(self.user)
